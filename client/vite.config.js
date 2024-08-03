3489c9a7164c2611534c55bcf7ef454bcd237cf1
iAmJitenderRawat <jitenderrawat39@gmail.com>
2024-08-04 00:27:23 +0530

    "Enable proxy in Vite config and remove console log in server app"

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": import.meta.env.VITE_BACKEND_URL,
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": import.meta.env.VITE_BACKEND_URL,
+    },
+  },
 });

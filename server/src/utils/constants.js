348a3bcdc7eff18b4ecabc10f2fe848f4f426b74
iAmJitenderRawat <jitenderrawat39@gmail.com>
2024-08-04 02:49:22 +0530

    add sameSite in cookie

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -2,10 +2,12 @@ export const accessCookieOptions = {
   httpOnly: true,
   secure: true,
   maxAge: process.env.ACCESS_TOKEN_MAX_AGE,
+  sameSite: process.env.SAME_SITE,
 };
 
 export const refreshCookieOptions = {
   httpOnly: true,
   secure: true,
   maxAge: process.env.REFRESH_TOKEN_MAX_AGE,
+  sameSite: process.env.SAME_SITE,
 };

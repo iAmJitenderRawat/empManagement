7997c3658e8b440c2947f21355451dd82af243e7
iAmJitenderRawat <jitenderrawat39@gmail.com>
2024-08-03 15:41:53 +0530

    "Update favicon and logo to eManager.png, and add proxy to vite.config.js"

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://employee-manager-x9d8.onrender.com',
+    }
+    },
 })

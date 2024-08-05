fb230a121e73b410dacefab99138c223a2e208aa
iAmJitenderRawat <jitenderrawat39@gmail.com>
2024-08-06 02:51:15 +0530

    debugging

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -15,69 +15,22 @@ import About from "./containers/About/About";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index:true,
-        path: "",
-        element: <LandingPage />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-      {
-        path: "login",
-        element: <LogIn />,
-      },
-      {
-        path: "register",
-        element: <Register />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "projects",
-        element: (
-          <ProtectedRoute>
-            <Projects />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "dashboard",
-        element: (
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        ),
-        children: [
-          {
-            path: "projects",
-            element: <Projects />,
-          },
-          {
-            path: "users",
-            element: <AllUsersPage />,
-          },
-        ],
-      },
-      {
-        path: "profile",
-        element: (
-          <ProtectedRoute>
-            <ProfilePage />
-          </ProtectedRoute>
-        ),
-        children: [
-          {
-            path: "edit",
-            element: <EditProfilePage />,
-          },
-        ],
-      },
-    ],
+    element: <LandingPage />,
+  },
+  {
+    path: "/login",
+    element: <LogIn />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
   },
 ]);
